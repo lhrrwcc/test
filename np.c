aa211d74e2d27014fcfa4ae82a3abe7737181caa
HLG <hlg@etale.com.ar>
2017-02-15 13:03:47 -0300

    third

--- np.c
+++ np.c
@@ -5,16 +5,24 @@
 void 
 b(void)
 {
-    printf("muehe\n");
-    a();
+    int i, j, k;
+    for(i=0; i<100; i++) { 
+        printf("eheum");
+        for(j=0; j<100; j++) {
+            printf("uhehene");
+            for(k=0; k<100; k++) {
+                printf("muehe");
+            }
+        }
+    }
+    
 }
 
 void 
 c(void)
 {
     b();
-    b();
-}
+ }
 
 void 
 a(void)
